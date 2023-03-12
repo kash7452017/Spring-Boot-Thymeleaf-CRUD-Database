f3ed819ef1dda8803851edc2f7f6b9d7efeb5c09
kash7452017 <kash7452017@gmail.com>
2023-03-12 17:09:06 +0800

    First Commit

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	
+	// Create a mapping for "/hello"
+	
+	@GetMapping("/hello")
+	public String sayHello(Model theModel) {
+		theModel.addAttribute("theDate", new java.util.Date());
+		
+		// We have Thymeleaf dependency in Maven POM
+		// Spring Boot will auto-configure to use Thymeleaf
+		return "helloworld";
+		// src/main/resources/templates/helloworld.html
+	}
+
+}
